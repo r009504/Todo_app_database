cca848c2d9cc117601b92528a348b3cdebeec4ee
balaram krishna <you@youraddress.com>
2020-05-07 19:19:24 +0530

    Add data to database

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 
 from . models import List
+from . forms import ListForms
 
 # Create your views here.
 
 
 def home(request):
-    all_items = List.objects.all
-    return render(request,'home.html',{'all_items':all_items})
+    if request.method == 'POST':
+        form = ListForms(request.POST or None )
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            return render(request, 'home.html', {'all_items': all_items})
+
+    else:
+         all_items = List.objects.all
+         return render(request, 'home.html', {'all_items': all_items})
+
+
 
 def about(request):
     return render(request,'about.html',{'Name': "Drishty and Khyaithy"})
