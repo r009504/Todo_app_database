86415499781c7430a8300f64dc5e2c3ebe31c6d3
balaram krishna <you@youraddress.com>
2020-05-08 19:04:52 +0530

    edit data program

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
 from . models import List
 from . forms import ListForms
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,6 +15,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request,("Item has been added to the list"))
             return render(request, 'home.html', {'all_items': all_items})
 
     else:
@@ -23,3 +26,49 @@ def home(request):
 
 def about(request):
     return render(request,'about.html',{'Name': "Drishty and Khyaithy"})
+
+
+def delete(request,list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ("Item has been deleted to the list"))
+    return redirect('home')
+
+
+
+def cross_off(request,list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+
+def cross_on(request,list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
+
+
+def edit(request,list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        form = ListForms(request.POST or None, instance=item )
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,("Item has been edited to the list"))
+            return redirect('home')
+
+    else:
+         item = List.objects.get(pk=list_id)
+         return render(request, 'edit.html', {'item': item})
+
+
+
+
+
+
+
+
+
